e9f9bfad39ae7526eb0d340ea2da8d90f97281dd
Roman Doroshenko <coachandconsult3@gmail.com>
2022-01-27 19:22:55 +0300

    modified StringChecker task2

--- src/main/java/tasktwo/StringChecker.java
+++ src/main/java/tasktwo/StringChecker.java
@@ -4,6 +4,7 @@ public class StringChecker {
     private final String RESPONSE_STRING = "Привет, ";
     private final String PATTERN = "Вячеслав";
     private final String WARNING = "Нет такого имени";
+
     public boolean responseToString(String name) {
         if (name.equals(PATTERN)) {
             System.out.println(RESPONSE_STRING + PATTERN);
