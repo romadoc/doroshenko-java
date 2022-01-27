a7f0bc95f31c310cee1297ed83d340444ae393c3
Roman Doroshenko <coachandconsult3@gmail.com>
2022-01-27 19:20:07 +0300

    StringChecker in task 2 was modified

--- src/main/java/tasktwo/StringChecker.java
+++ src/main/java/tasktwo/StringChecker.java
@@ -3,12 +3,13 @@ package tasktwo;
 public class StringChecker {
     private final String RESPONSE_STRING = "Привет, ";
     private final String PATTERN = "Вячеслав";
+    private final String WARNING = "Нет такого имени";
     public boolean responseToString(String name) {
         if (name.equals(PATTERN)) {
             System.out.println(RESPONSE_STRING + PATTERN);
             return true;
         } else {
-            System.out.println("Нет такого имени");
+            System.out.println(WARNING);
         }
         return false;
     }
