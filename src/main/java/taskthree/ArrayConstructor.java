c6bcdc5d50a8b556e908f629c418d917087e70fd
Roman Doroshenko <coachandconsult3@gmail.com>
2022-02-02 17:57:40 +0300

    final commit

--- src/main/java/taskthree/ArrayConstructor.java
+++ src/main/java/taskthree/ArrayConstructor.java
@@ -3,6 +3,9 @@ package taskthree;
 public class ArrayConstructor {
 
     public int[] createArray(int n) {
+        if (n < 0) {
+            n = n * -1;
+        }
         int[] array = new int[n];
         for (int i = 0; i < n; i++) {
             int x = (int) (Math.random() * 100);
